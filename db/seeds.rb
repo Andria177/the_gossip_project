bd2969c18e8cc522cf492b6ad3fdd45672aa2ec0
Andria177 <andriarzk@gmail.com>
2020-05-05 03:15:26 +0300

    db1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+# 10 city
+
+10.times do
+  city = City.create!(name: Faker::Address.city, zip_code: Faker::Address.zip_code)
+end
+
+# Génération de 10 users
+10.times do
+  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::TvShows::GameOfThrones.quote, email: Faker::Internet.email, age: Faker::Number.within(range: 15..45), city_id: Faker::Number.within(range: 1..10))
+end
+
+# Génération de 20 gossips
+20.times do
+  gossip = Gossip.create!(title: Faker::Dessert.flavor, content: Faker::ChuckNorris.fact, user_id: Faker::Number.within(range: 1..10))
+end
